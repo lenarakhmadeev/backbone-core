4f7b077741565d0f9210ad5c0cd0b23280a4ae07
Lenar Akhmadeev <lenarakhmadeev@gmail.com>
2013-05-04 21:40:41 +0600

    Model#dispose

--- model.js
+++ model.js
@@ -25,6 +25,37 @@ define(function (require) {
 		unary: function (name, operation) {
 			var value = this.get(name);
 			this.set(name, operation(value));
+		},
+
+		dispose: function () {
+			// todo: Закончить
+			return;
+			if (this.disposed) return;
+
+			// Finished.
+			this.disposed = true;
+
+			// Unbind all global event handlers.
+			this.unsubscribeAllEvents();
+
+			// Unbind all referenced handlers.
+			this.stopListening();
+
+			// Remove all event handlers on this module.
+			this.off();
+
+
+			// Remove the collection reference, internal attribute hashes
+			// and event handlers.
+				var properties = [
+					'collection',
+					'attributes', 'changed',
+					'_escapedAttributes', '_previousAttributes',
+					'_silent', '_pending',
+					'_callbacks'
+				];
+
+//			delete this[prop] for prop in properties
 		}
 
 	});
